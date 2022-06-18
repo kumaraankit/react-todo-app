1e945dee5aa8292b8173b3191ff9f8db3f2d866e
ANKIT KUMAR <64415303+kumaraankit@users.noreply.github.com>
2022-06-18 23:30:59 +0530

    addedinput component (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import "./App.css";
+import Input from "./components/Input";
 
 function App() {
   return (
     <div className="App">
       <h1> Todo App</h1>
+      <Input />
     </div>
   );
 }
