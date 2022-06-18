21c145c4bb2214753210fe1bcf237144726a3458
ANKIT KUMAR <64415303+kumaraankit@users.noreply.github.com>
2022-06-18 23:47:05 +0530

    onchange event (#4)

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Input:any=()=> {
+const Input: any = () => {
+  const [todo, setTodo] = useState("");
+  const handleChange = (event: any) => {
+    setTodo(event.target.value);
+  };
   return (
     <div>
-        <input type="text"/>
-        <button>Add</button>
+      <input type="text" onChange={(e) => handleChange(e)} value={todo} />
+      <button>Add</button>
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
